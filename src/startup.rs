1f7de77b20890b0ffe4bde5902110b3ffc576efe
xiaomeng79 <496096100@qq.com>
2020-12-09 15:12:19 +0800

    feat:支持tracing和loging(#10) fixed #10

--- src/startup.rs
+++ src/startup.rs
@@ -3,11 +3,14 @@ use actix_web::dev::Server;
 use actix_web::{web, App, HttpServer};
 use sqlx::PgPool;
 use std::net::TcpListener;
+use tracing_actix_web::TracingLogger;
 
 pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Error> {
     let db_pool = web::Data::new(db_pool);
     let server = HttpServer::new(move || {
         App::new()
+            // Middlewares are added using the `wrap` method on `App`
+            .wrap(TracingLogger)
             .route("/subscriptions", web::post().to(subscriptions::subscribe))
             .route("/health_check", web::get().to(health_check::health_check))
             // Register the connection as part of the application state
